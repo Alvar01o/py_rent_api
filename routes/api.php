0b193fca2ef2429280130e551d7c5cd3d85aebd2
sariavaal <sariavaal@gmail.com>
2021-04-12 11:48:52 -0400

    validacion,actualizacion de propiedades

--- routes/api.php
+++ routes/api.php
@@ -19,6 +19,7 @@ use App\Http\Controllers\RealStateController;
 
 Route::middleware('auth:sanctum')->group( function () {
     Route::post('/real_states' , [RealStateController::class, 'store']);
+    Route::put('/real_states/{id}' , [RealStateController::class, 'update']);
     Route::delete('/real_states/{id}' , [RealStateController::class, 'destroy']);
     Route::get('/real_states/{id}' , [RealStateController::class, 'show']);
     Route::get('/real_states' , [RealStateController::class, 'index']);
