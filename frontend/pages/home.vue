6246b3f43a484ab991762498885bf04145ef7b11
sariavaal <sariavaal@gmail.com>
2021-05-09 15:10:44 -0400

    codigo de ejemplo

---
+++ frontend/pages/home.vue
@@ -0,0 +1,43 @@
+<template>
+<div>
+    <div class="border row">
+        <nav class="navbar navbar-expand-lg navbar-light bg-transparent">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Navbar</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li v-for="(valor, index) in opciones" :key="index" class="nav-item">
+                    <a href="#" class="nav-link">{{valor}}</a>
+                </li>
+            </ul>
+            </div>
+        </div>
+        </nav>
+    </div>
+
+</div>
+</template>
+
+<script>
+export default {
+    layout: null,
+    data(){
+        return {
+            opciones: [
+                'Opcion1',
+                'Opcion2',
+                'Opcion3',
+                'Opcion4',
+                'Opcion5',
+            ]
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
